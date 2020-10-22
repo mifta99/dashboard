790a401ca8ae9191aa9a95f072ddd30e01e5f967
Reindra <reindrairawan@gmail.com>
2020-10-22 15:57:23 +0700

    import library

--- app/src/main/java/mifta/code/dispendukproject1/AktaKelahiranActivity.java
+++ app/src/main/java/mifta/code/dispendukproject1/AktaKelahiranActivity.java
@@ -3,6 +3,14 @@ package mifta.code.dispendukproject1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.github.ybq.android.spinkit.sprite.CircleSprite;
+import com.github.ybq.android.spinkit.style.Circle;
+import com.github.ybq.android.spinkit.style.DoubleBounce;
+
+import static mifta.code.dispendukproject1.Colors.colors;
 
 public class AktaKelahiranActivity extends AppCompatActivity {
 
