7798e9a09c98d7fb393b468e3ba54169a31a4c7d
Miftahul Hasanah <hasanahm341@gmail.com>
2020-11-01 11:03:26 +0700

    deleting unecessary custom layout

--- app/src/main/java/mifta/code/dispendukproject1/adapter/KkAdapter.java
+++ app/src/main/java/mifta/code/dispendukproject1/adapter/KkAdapter.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -36,6 +39,12 @@ public class KkAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         tampil result = results.get(position);
         myHolder.nama_kec.setText("KECAMATAN "+result.getNAMA_KEC());
         myHolder.total_kec.setText(result.getJUMLAH());
+        myHolder.jenis.setText("Kartu Keluarga");
+        Glide.with(myHolder.itemView.getContext())
+                .load(R.drawable.ic_kk)
+                .placeholder(R.drawable.ic_loading)
+                .error(R.drawable.ic_loading)
+                .into(myHolder.ic);
     }
 
     @Override
@@ -44,12 +53,15 @@ public class KkAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     }
 
     class MyHolder extends RecyclerView.ViewHolder {
-        TextView nama_kec, total_kec;
+        TextView nama_kec, total_kec, jenis;
+        ImageView ic;
 
         public MyHolder(View itemView) {
             super(itemView);
             nama_kec = itemView.findViewById(R.id.tvx_kec);
             total_kec = itemView.findViewById(R.id.tvx_totalKec);
+            jenis = itemView.findViewById(R.id.tvx_jenis);
+            ic= itemView.findViewById(R.id.imx_icpict);
         }
     }
 }
