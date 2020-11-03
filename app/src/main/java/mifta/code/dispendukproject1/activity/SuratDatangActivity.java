ad46276e3aea32b496168c8e089653d509105d53
Miftahul Hasanah <hasanahm341@gmail.com>
2020-11-03 09:06:31 +0700

    progressbar for surat datang

--- app/src/main/java/mifta/code/dispendukproject1/activity/SuratDatangActivity.java
+++ app/src/main/java/mifta/code/dispendukproject1/activity/SuratDatangActivity.java
@@ -1,16 +1,28 @@
 package mifta.code.dispendukproject1.activity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.ybq.android.spinkit.style.Circle;
+
 import mifta.code.dispendukproject1.R;
 
+import static mifta.code.dispendukproject1.api.Colors.colors;
+
 public class SuratDatangActivity extends AppCompatActivity {
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_surat_datang);
+
+        progressBar = findViewById(R.id.progressBar);
+
+        Circle circle = new Circle();
+        circle.setColor(colors[8]);
+        progressBar.setIndeterminateDrawable(circle);
     }
 }
