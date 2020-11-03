dc6a7559848267c86beda3e4d7c206d7bc192489
Miftahul Hasanah <hasanahm341@gmail.com>
2020-11-03 10:10:10 +0700

    menu dashboard untuk user capil

--- app/src/main/java/mifta/code/dispendukproject1/activity/DashboardActivity.java
+++ app/src/main/java/mifta/code/dispendukproject1/activity/DashboardActivity.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,7 +22,8 @@ import mifta.code.dispendukproject1.R;
 
 public class DashboardActivity extends AppCompatActivity implements View.OnClickListener {
     TextView txtUser, txt_ktp, txt_kk, txt_akta, txt_pindah, txt_datang, txt_biodata;
-    ImageView foto, kk, ktp, akta, datang, pindah, biodata;
+    ImageView foto, kk, ktp, akta, datang, pindah, biodata, kelahiran, kematian, perkawinan, perceraian;
+    TableRow tbl1, tbl2,tbl3, tbl4, tbl5, tbl6, tbl7, tbl8;
     private Menu action;
 
     @Override
@@ -49,25 +51,37 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
         txt_pindah = findViewById(R.id.txt_pindah);
         txt_datang = findViewById(R.id.txt_datang);
         txt_biodata = findViewById(R.id.txt_biodata);
+        kelahiran = findViewById(R.id.im_akta_kelahiran);
+        kematian = findViewById(R.id.im_akta_kematian);
+        perkawinan = findViewById(R.id.im_akta_perkawinan);
+        perceraian = findViewById(R.id.im_akta_perceraian);
+        tbl1 = findViewById(R.id.tbl1);
+        tbl2 = findViewById(R.id.tbl2);
+        tbl3 = findViewById(R.id.tbl3);
+        tbl4 = findViewById(R.id.tbl4);
+        tbl5 = findViewById(R.id.tbl5);
+        tbl6 = findViewById(R.id.tbl6);
+        tbl7 = findViewById(R.id.tbl7);
+        tbl8 = findViewById(R.id.tbl8);
+
 
         final String nama_ = aud.get(2);
         final String status_ = aud.get(1);
         final String foto_ = aud.get(4);
 
         if (status_.equals("dafduk")){
-            akta.setVisibility(View.GONE);
-            txt_akta.setVisibility(View.GONE);
+            akta.setVisibility(View.INVISIBLE);
+            txt_akta.setVisibility(View.INVISIBLE);
         }else if (status_.equals("capil")){
-            kk.setVisibility(View.GONE);
-            txt_kk.setVisibility(View.GONE);
-            ktp.setVisibility(View.GONE);
-            txt_ktp.setVisibility(View.GONE);
-            biodata.setVisibility(View.GONE);
-            txt_biodata.setVisibility(View.GONE);
-            pindah.setVisibility(View.GONE);
-            txt_pindah.setVisibility(View.GONE);
-            datang.setVisibility(View.GONE);
-            txt_datang.setVisibility(View.GONE);
+            tbl1.setVisibility(View.GONE);
+            tbl2.setVisibility(View.GONE);
+            tbl3.setVisibility(View.GONE);
+            tbl4.setVisibility(View.GONE);
+
+            tbl5.setVisibility(View.VISIBLE);
+            tbl6.setVisibility(View.VISIBLE);
+            tbl7.setVisibility(View.VISIBLE);
+            tbl8.setVisibility(View.VISIBLE);
         }
 
         txtUser.setText("HALO " + nama_);
@@ -81,6 +95,10 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
         datang.setOnClickListener(this);
         pindah.setOnClickListener(this);
         biodata.setOnClickListener(this);
+        kelahiran.setOnClickListener(this);
+        kematian.setOnClickListener(this);
+        perkawinan.setOnClickListener(this);
+        perceraian.setOnClickListener(this);
     }
 
     @Override
@@ -109,6 +127,22 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
             case R.id.im_biodata:
                 Intent g = new Intent(DashboardActivity.this, BiodataActivity.class);
                 startActivity(g);
+                break;
+            case R.id.im_akta_kelahiran:
+                Intent h = new Intent(DashboardActivity.this, AktaKelahiranActivity.class);
+                startActivity(h);
+                break;
+            case R.id.im_akta_kematian:
+                Intent i = new Intent(DashboardActivity.this, AktaKematianActivity.class);
+                startActivity(i);
+                break;
+            case R.id.im_akta_perkawinan:
+                Intent j = new Intent(DashboardActivity.this, AktaPerkawinanActivity.class);
+                startActivity(j);
+                break;
+            case R.id.im_akta_perceraian:
+                Intent k = new Intent(DashboardActivity.this, AktaPerceraianActivity.class);
+                startActivity(k);
                 break;
         }
     }
