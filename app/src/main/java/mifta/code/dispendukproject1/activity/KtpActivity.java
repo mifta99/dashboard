6bc9d2e571ea76dd12239b303d450e7184f1fe35
Miftahul Hasanah <hasanahm341@gmail.com>
2020-11-06 10:10:54 +0700

    menu pencetakan ktp

--- app/src/main/java/mifta/code/dispendukproject1/activity/KtpActivity.java
+++ app/src/main/java/mifta/code/dispendukproject1/activity/KtpActivity.java
@@ -1,16 +1,41 @@
 package mifta.code.dispendukproject1.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import mifta.code.dispendukproject1.R;
 
-public class KtpActivity extends AppCompatActivity {
+public class KtpActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageView perekaman, pencetakan;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ktp);
+
+        perekaman = findViewById(R.id.bt_perekaman);
+        pencetakan = findViewById(R.id.bt_pencetakan);
+
+        perekaman.setOnClickListener(this);
+        pencetakan.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bt_pencetakan:
+                Intent a = new Intent(KtpActivity.this, KtpPencetakanActivity.class);
+                startActivity(a);
+                break;
+            case R.id.bt_perekaman:
+                Intent b = new Intent(KtpActivity.this, KtpPerekamanActivity.class);
+                startActivity(b);
+                break;
+        }
     }
 }
--- app/src/main/java/mifta/code/dispendukproject1/adapter/KtpPencetakanAdapter.java
+++ app/src/main/java/mifta/code/dispendukproject1/adapter/KtpPencetakanAdapter.java
@@ -1,4 +1,67 @@
 package mifta.code.dispendukproject1.adapter;
 
-public class KtpPencetakanAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import mifta.code.dispendukproject1.R;
+import mifta.code.dispendukproject1.api.tampil;
+
+public class KtpPencetakanAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private Context context;
+    private List<tampil> results;
+
+    public KtpPencetakanAdapter(Context context, List<tampil> results) {
+        this.context = context;
+        this.results = results;
+
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.custom_layout, null);
+        KtpPencetakanAdapter.MyHolder holder = new KtpPencetakanAdapter.MyHolder(v);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        KtpPerekamanAdapter.MyHolder myHolder = (KtpPerekamanAdapter.MyHolder) holder;
+        tampil result = results.get(position);
+        myHolder.nama_kec.setText("KECAMATAN " + result.getNAMA_KEC());
+        myHolder.total_kec.setText(result.getJUMLAH());
+        myHolder.jenis.setText("KTP Pencetakan");
+        Glide.with(myHolder.itemView.getContext())
+                .load(R.drawable.ic_ktp)
+                .placeholder(R.drawable.ic_loading)
+                .error(R.drawable.ic_loading)
+                .into(myHolder.ic);
+    }
+
+    @Override
+    public int getItemCount() {
+        return results.size();
+    }
+
+    class MyHolder extends RecyclerView.ViewHolder {
+        TextView nama_kec, total_kec, jenis;
+        ImageView ic;
+
+        public MyHolder(View itemView) {
+            super(itemView);
+            nama_kec = itemView.findViewById(R.id.tvx_kec);
+            total_kec = itemView.findViewById(R.id.tvx_totalKec);
+            jenis = itemView.findViewById(R.id.tvx_jenis);
+            ic = itemView.findViewById(R.id.imx_icpict);
+        }
+    }
 }
