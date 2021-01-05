8bb0f9a81fd4f736e461146f2538f5777cac03de
Miftahul Hasanah <hasanahm341@gmail.com>
2021-01-05 15:04:23 +0700

    cardview intent

--- app/src/main/java/mifta/code/dispendukproject1/adapter/BiodataAdapter.java
+++ app/src/main/java/mifta/code/dispendukproject1/adapter/BiodataAdapter.java
@@ -1,12 +1,15 @@
 package mifta.code.dispendukproject1.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -15,6 +18,7 @@ import java.util.List;
 
 import mifta.code.dispendukproject1.R;
 import mifta.code.dispendukproject1.api.tampil;
+import mifta.code.dispendukproject1.bio_1jat;
 
 public class BiodataAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private Context context;
@@ -56,6 +60,17 @@ public class BiodataAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
                 .placeholder(R.drawable.ic_loading)
                 .error(R.drawable.ic_loading)
                 .into(myHolder.ic);
+        myHolder.crd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), bio_1jat.class);
+                Integer no_kec = Integer.valueOf(result.getNO_KEC());
+                intent.putExtra("no_kec", no_kec);
+                Toast.makeText(context, String.valueOf(no_kec), Toast.LENGTH_SHORT).show();
+                context.startActivity(intent);
+            }
+        });
+
     }
 
     @Override
@@ -66,6 +81,7 @@ public class BiodataAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
     class MyHolder extends RecyclerView.ViewHolder {
         TextView nama_kec, total_kec, jenis;
         ImageView ic;
+        CardView crd;
 
         public MyHolder(View itemView) {
             super(itemView);
@@ -73,6 +89,7 @@ public class BiodataAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
             total_kec = itemView.findViewById(R.id.tvx_totalKec);
             jenis = itemView.findViewById(R.id.tvx_jenis);
             ic = itemView.findViewById(R.id.imx_icpict);
+            crd = itemView.findViewById(R.id.crd);
         }
     }
 
